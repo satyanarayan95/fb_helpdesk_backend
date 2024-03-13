065ee0441fd5400e0e93134a80ca65e6b46eb0b0
Satya Narayan Patra <70203253+satyanarayan95@users.noreply.github.com>
2024-03-13 19:22:19 +0000

    done verify

--- routes/facebookRoutes.js
+++ routes/facebookRoutes.js
@@ -3,7 +3,7 @@ const { webhookVerify, messageDlr } = require('../controllers/facebookController
 const router = express.Router();
 
 // webhook
-router.get('/webhook-dlr', webhookVerify);
+router.get('/verify-webhook', webhookVerify);
 router.post('/webhook-dlr', messageDlr);
 
 
